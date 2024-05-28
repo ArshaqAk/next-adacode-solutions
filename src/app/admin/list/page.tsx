3744bdea9d71ce9d074c9153a87498b77d6a3ac6
arshaq <arshaqakard@gmail.com>
2024-05-28 03:50:29 -0700

    cmp up

--- src/app/admin/list/page.tsx
+++ src/app/admin/list/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { FaPhoneAlt, FaWhatsapp, FaTrash } from "react-icons/fa";
 
@@ -12,53 +12,58 @@ interface Props {
   studentList: Student[];
 }
 
-const List = ({ studentList }) => {
+const List: React.FC<Props> = ({ studentList }) => {
   console.log(studentList);
-  
-  const handleDelete = (index: any) => {
+
+  const handleDelete = (index: number) => {
     console.log("Delete");
   };
-  const handleStatus = ({ item }) => {};
+
+  const handleStatus = (item: Student) => {};
+
   return (
     <div className="studentlist_container">
       <table>
-        <tr>
-          <th>Id</th>
-          <th>Student Name</th>
-          <th>Phone Number</th>
-          <th>Status</th>
-          <th>Actions</th>
-        </tr>
-        {/* @ts-ignore */}
-        {studentList.map((item, index) => (
-          <tr key={index}>
-            <td>{index}</td>
-            <td>{item.studentname}</td>
-            <td>{item.phonenumber}</td>
-            <td>{item.isCalled ? <>Called</> : <>Not Called</>}</td>
-            <td className="table_icons_container">
-              <a href={`tel:+91${item.phonenumber}`}>
-                <button
-                  className="phone-icon"
-                  onClick={() => handleStatus(item.phonenumber)}
-                >
-                  <FaPhoneAlt />
-                </button>
-              </a>
-              <a href={`http://wa.me/91${item.phonenumber}`}>
-                <button className="whatsapp-icon">
-                  <FaWhatsapp />
-                </button>
-              </a>
-              <button
-                onClick={() => handleDelete(index)}
-                className="delete-icon"
-              >
-                <FaTrash />
-              </button>
-            </td>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Student Name</th>
+            <th>Phone Number</th>
+            <th>Status</th>
+            <th>Actions</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {studentList.map((item, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{item.studentname}</td>
+              <td>{item.phonenumber}</td>
+              <td>{item.isCalled ? <>Called</> : <>Not Called</>}</td>
+              <td className="table_icons_container">
+                <a href={`tel:+91${item.phonenumber}`}>
+                  <button
+                    className="phone-icon"
+                    onClick={() => handleStatus(item)}
+                  >
+                    <FaPhoneAlt />
+                  </button>
+                </a>
+                <a href={`http://wa.me/91${item.phonenumber}`}>
+                  <button className="whatsapp-icon">
+                    <FaWhatsapp />
+                  </button>
+                </a>
+                <button
+                  onClick={() => handleDelete(index)}
+                  className="delete-icon"
+                >
+                  <FaTrash />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
